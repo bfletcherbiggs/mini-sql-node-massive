2fe8aa03361749b43af87a7246fc533cae48546b
Ben Biggs <bfletcherbiggs@gmail.com>
2017-03-21 16:43:56 -0500

    done

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
 
-var app = module.exports = express();
+var app = express();
+var connectionString = "postgres://benjaminbiggs@localhost/sandbox";
+var massiveInstance = massive.connectSync({connectionString : connectionString})
+app.set('db', massiveInstance);
+module.exports = app
+//Possibly local files need to be required later
+var controller = require('./controller.js');
+
+controller.getPlanes();
+
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
+  console.log("Successfully listening on : 3000")
 })
-
